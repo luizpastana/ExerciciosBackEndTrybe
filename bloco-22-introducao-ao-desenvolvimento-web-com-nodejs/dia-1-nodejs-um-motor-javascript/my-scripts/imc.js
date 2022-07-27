903490d15161b9f02d13048f8d1fbdcb0722e680
luizpastana <lcsp84@gmail.com>
2022-07-26 23:07:52 -0300

    Exercicio 05: Switch sempre retorna o valor default. Falta corrigir

--- bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -11,12 +11,22 @@ function calculaImc(peso, altura) {
   return imc;
 }
 
+const situaçãoImc = (imc) => {
+  switch(imc) {
+  case imc < 18.5:
+    return "Abaixo do Peso (magreza)";
+  default:
+    return "Obesidade";
+  }
+}
+
 function main() {
-  const peso = readline.questionInt('Qual o seu peso? (em kg) ');
+  const peso = readline.questionFloat('Qual o seu peso? (em kg) ');
   const altura = readline.questionInt('Qual a sua altura? (em cm) ');
   
   const imc = calculaImc(peso, altura);
 
+  console.log(situaçãoImc(imc));
   console.log(`IMC: ${imc.toFixed(2)}`);
 }
 
