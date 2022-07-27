133c4650cb95c4f2bcbd29036219f92f117eaff7
luizpastana <lcsp84@gmail.com>
2022-07-27 20:36:30 -0300

    Resolve exercicio 01 e 02

---
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/index.js
@@ -0,0 +1,44 @@
+function doMath(a, b, c) {
+  return new Promise((resolve, reject) => {
+    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+      return reject(new Error('Informe apenas números'));}
+
+    const result = (a + b) * c;
+
+    if (result < 50) {
+      return reject(new Error('Valor muito baixo'));
+    }
+    resolve(result); 
+  });
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100 + 1);
+}
+
+function callDoMath() {
+  /* Criamos um novo array de 3 posições
+   * e utilizamos o `map` para gerar um número aleatório
+   * para cada posição do Array
+   */
+  const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber);
+  console.log(randomNumbers);
+
+  return randomNumbers;
+}
+
+doMath(...callDoMath())
+  .then((result) => console.log(result))
+  .catch((err) => console.error(err.message));
+
+// doMath(10, 10, 10)
+//   .then((resolve) => console.log(resolve))
+//   .catch((error) => console.log(error.message));
+
+// doMath(1, 1, 'a')
+//   .then((resolve) => console.log(resolve))
+//   .catch((error) => console.log(error.message));
+
+// doMath(1, 1, 1)
+//   .then((resolve) => console.log(resolve))
+//   .catch((error) => console.log(error.message));
