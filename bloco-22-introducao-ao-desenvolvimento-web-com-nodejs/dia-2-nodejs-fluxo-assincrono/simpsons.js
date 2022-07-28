723ab33107901ab90d6130d52303728ecda20833
luizpastana <lcsp84@gmail.com>
2022-07-27 21:15:53 -0300

    Exercicio 05 - filtra um personagem pelo id

--- bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/simpsons.js
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/simpsons.js
@@ -10,6 +10,26 @@ function readAll() {
   });
 }
 
+
+async function getSimpsonById(id) {
+  const fileContent = await fs
+    .readFile('./Trybe_e_os_simpsons.json', 'utf-8');
+  
+  const simpsons = JSON.parse(fileContent);
+
+  const chosenSimpson = simpsons.find((simpson) => Number(simpson.id) === id);
+
+     if (!chosenSimpson) {
+       /* A palavra-chave `throw` dispara um erro que deve ser tratado por quem chamou nossa função.
+        * Em funções `async`, utilizar `throw` faz com que a Promise seja rejeitada,
+        * tendo como motivo o que passarmos para o `throw`.
+        * Ou seja, a linha abaixo rejeita a Promise da nossa função com um Erro que tem a mensagem 'id não encontrado'
+        */
+       throw new Error('id não encontrado');
+     }
+     return chosenSimpson;
+}
+
 // async function readAllComAsyncAwait() {
 //   const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
 //   const simpsons = JSON.parse(fileContent);
@@ -18,8 +38,9 @@ function readAll() {
 //   strings.forEach((string) => console.log(string));
 // }
 
-function main() {
-  readAll();
+async function main() {
+  const simpson = await getSimpsonById(3);
+  console.log(simpson);
 }
 
 main();
